f26832b249d21435305f0106643b9e67555336c8
Abdullah-Tamki <abdullahaltamki@gmail.com>
2025-03-24 01:59:56 +0000

    fix views

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".couter-number");
 async function updateCounter(){
     let response = await  fetch("https://qios5trvjqmfolyxy4aymupjg40ljqsw.lambda-url.eu-west-2.on.aws/"); // calling the lambda function
     let data = await response.json(); // getting the respone with views number
-    counter.innerHTML = ` Views: ${data}`
+    counter.innerHTML = ` Views: ${data.views}`
 }
 
 updateCounter();
